cf8866225022c862846d2b314e337f399d63cde2
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-02-12 17:24:02 +0700

    read firebase

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,42 @@
         </button>
       </div>
     </div>
+    <div>
+      <h2>
+        Read from Firestore.
+      </h2>
+      <div>
+        <button @click="readFromFirestore" :disabled="readSuccessful">
+          <span v-if="!readSuccessful">Read now</span>
+          <span v-else>Successful!</span>
+        </button>
+        <p>{{ text }}</p>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import { fireDb } from '~/plugins/firebase.js'
 export default {
+  async asyncData({ app, params, error }) {
+    const ref = fireDb.collection('test').doc('test')
+    let snap
+    try {
+      snap = await ref.get()
+    } catch (e) {
+      // TODO: error handling
+      console.error(e)
+    }
+    return {
+      text: snap.data().text
+    }
+  },
   data() {
     return {
-      writeSuccessful: false
+      writeSuccessful: false,
+      readSuccessful: false,
+      text: ''
     }
   },
   methods: {
@@ -36,6 +63,18 @@ export default {
       }
       this.writeSuccessful = true
     }
+  },
+  async readFromFirestore() {
+    const ref = fireDb.collection('test').doc('test')
+    let snap
+    try {
+      snap = await ref.get()
+    } catch (e) {
+      // TODO: error handling
+      console.error(e)
+    }
+    this.text = snap.data().text
+    this.readSuccessful = true
   }
 }
 </script>
